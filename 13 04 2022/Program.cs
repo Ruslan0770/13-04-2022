cd78906a8b9977105e5ccc23014f955dd4d7c278
Ruslan0770 <ruslania@code.edu.az>
2022-04-12 14:11:21 -0700

    notebook

---
+++ 13 04 2022/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _13_04_2022.models
+{ 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Product avg = new Product("notebook");
+
+
+            avg.count = 10;
+            avg.price = 2000;
+
+            avg.sell();
+            avg.sell();
+
+
+            Console.WriteLine($"{avg.count} {avg.Totalincome}");
+
+        }
+    }
+}
---
+++ 13 04 2022/models/product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _13_04_2022.models
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double price { get; set; }
+
+        public int count { get; set; }
+        public double Totalincome { get; set; }
+
+        public Product(string name)
+        {
+            Name = name;
+            Console.WriteLine(name);
+
+
+        }
+
+        public void sell()
+        {
+
+            if (count > 0)
+            {
+                count--;
+                Totalincome += price;
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("mehsul bitib");
+            }
+
+
+        }
+
+    }
+}
